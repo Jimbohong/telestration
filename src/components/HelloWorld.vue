b264fc44f34d1c1c375f1bdbf9156ba9333b3707
Cyril <cyril@aja.com.tw>
2020-03-03 12:16:12 +0800

    update topics

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,13 @@
 <template>
   <div class="hello">
     <h1>傳情畫意</h1>
+    <h3>題庫</h3>
+    <div v-for="item in topic" :key="item">
+      {{item}}
+    </div>
+    <hr>
+    <h3>結果: {{this.result}}</h3>
+    <button @click="randomTopic"><h1>Random Topic</h1></button>
   </div>
 </template>
 
@@ -9,7 +16,21 @@ export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  data() {
+    return{
+      topic: ['習近平','金正恩','耿爽','小虎隊','館長','隱形人','洪金寶','scrum of scrum','紫禁之巔','夜市人生','寄生上流','R&R','拉斯維加斯','韓國','路易莎','婚宴蟑螂'],
+      result: ''
+    }
+  },
+  methods:{
+    randomTopic(){
+      let result = Math.floor(Math.random() * this.topic.length);
+      console.log(this.topic[result]);
+      this.result = this.topic[result];
+    }
   }
+  
 }
 </script>
 
